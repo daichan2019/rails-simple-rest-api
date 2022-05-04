d7898c3e15ee771291a86926079e0ab7e451316f
daichi_sato <daichiexperience@gmail.com>
2022-05-04 16:07:58 +0900

    [add] controller

---
+++ app/controllers/api/v1/auth/users_controller.rb
@@ -0,0 +1,42 @@
+require_dependency 'api/v1/application_controller'
+module Api
+  module V1
+    module Auth
+      class UsersController < V1::ApplicationController
+        skip_before_action :authenticate_user
+
+        def create
+          FirebaseIdToken::Certificates.request
+          raise ArgumentError, 'BadRequest Parameter' if payload.blank?
+          @user = User.find_or_initialize_by(uid: payload['sub']) do |user|
+            user.name = payload['name']
+            user.email = payloard['email']
+          end
+          if @user.save
+            render json: @user, status: :ok
+          else
+            render json: @user.errors, status: :unprocessable_entity
+          end
+        end
+
+        def show
+          @user = User.find(params[:id])
+          render json: @user
+        end
+
+        def update
+        end
+
+        private
+
+        def token
+          params[:token] || token_from_request_headers
+        end
+
+        def payload
+          @payload ||= FirebaseIdToken::Signature.verify token
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace 'api' do
+    namespace 'v1' do
+      namespace 'auth' do
+        resources :users, only: %i[create show update]
+      end
+    end
+  end
 end
